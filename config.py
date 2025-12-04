36029de67793a3f69dd6b580203b0199e15d6fcf
Illhm <gusmif@ilhamxander.web.id>
2025-12-05 01:51:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ Copy this file to config.py and fill in your credentials
 # TELEGRAM API CREDENTIALS (REQUIRED)
 # ============================================================================
 # Get these from https://my.telegram.org/apps
-API_ID = 961780 # Example: "12345678"
+API_ID = "961780" # Example: "12345678"
 API_HASH = "bbbfa43f067e1e8e2fb41f334d32a6a7"  # Example: "0123456789abcdef0123456789abcdef"
 
 # Get this from @BotFather on Telegram
